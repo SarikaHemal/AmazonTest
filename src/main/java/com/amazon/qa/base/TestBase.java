53bb3cf719f9221583b8438e060f6b0e7b05b1a2
sarik <sarikabamania13@gmail.com>
2020-03-28 10:07:42 +1300

    browser initialization

--- src/main/java/com/amazon/qa/base/TestBase.java
+++ src/main/java/com/amazon/qa/base/TestBase.java
@@ -4,8 +4,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class TestBase {
 	static WebDriver driver;
@@ -23,6 +27,26 @@ public class TestBase {
 		catch(IOException e) {
 			e.printStackTrace();
 		}
+	}
+	public static void intialization() {
+		String browserName =prop.getProperty("browser");
+		if (browserName.equals("Chrome")) {
+			System.setProperty("webdriver.chrome.driver","C:\\ChomeDriver\\new\\chromedriver.exe");
+			driver =new ChromeDriver();//launch Chrome
+		}
+		else if (browserName.equals("Firefox")) {
+			System.setProperty("webdriver.gecko.driver","C:\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+			driver =new FirefoxDriver();//launch firefox
+		}
+		else if (browserName.equals("IE")) {
+			System.setProperty("webdriver.ie.driver","C:\\internetexplorerldriver\\iedriverserver.exe");
+			driver = new InternetExplorerDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.amazon.com");
 	}
 		
 
